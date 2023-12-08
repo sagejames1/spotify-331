35dba427c55041f86b4997fad297b50ddb672dbd
sagejames1 <119362293+sagejames1@users.noreply.github.com>
2023-12-08 14:05:50 -0700

    Update main.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,39 +1,57 @@
 
 function showLastMonthTracks() {
-     document.getElementById('lastMonth').style.display = 'block';
-     document.getElementById('6months').style.display= 'none';
-     document.getElementById('allTime').style.display = 'none';
- }
+    document.getElementById('lastMonth').style.display = 'block';
+    document.getElementById('6months').style.display= 'none';
+    document.getElementById('allTime').style.display = 'none';
+}
 
- function showLastSixMonthsTracks() {
-     document.getElementById('lastMonth').style.display = 'none';
-     document.getElementById('6months').style.display = 'block';
-     document.getElementById('allTime').style.display = 'none';
- }
+function showLastSixMonthsTracks() {
+    document.getElementById('lastMonth').style.display = 'none';
+    document.getElementById('6months').style.display = 'block';
+    document.getElementById('allTime').style.display = 'none';
+}
 
- function showAllTimeTracks() {
-     document.getElementById('lastMonth').style.display = 'none';
-     document.getElementById('6months').style.display = 'none';
-     document.getElementById('allTime').style.display = 'block';
- }   
- 
- function spotifyLogout() {
-     const logoutUrl = 'https://accounts.spotify.com/en/logout';
-     const spotifyLogoutWindow = window.open(logoutUrl, 'Spotify Logout', 'width=700,height=500,top=40,left=40');
-     
-     setTimeout(() => {
-         spotifyLogoutWindow.close();
-         window.location.href = '/'; 
-     }, 1000);
- }
+function showAllTimeTracks() {
+    document.getElementById('lastMonth').style.display = 'none';
+    document.getElementById('6months').style.display = 'none';
+    document.getElementById('allTime').style.display = 'block';
+}   
 
- function downloadAsPNG() {
-    const element = document.getElementById('download');
+function spotifyLogout() {
+    const logoutUrl = 'https://accounts.spotify.com/en/logout';
+    const spotifyLogoutWindow = window.open(logoutUrl, 'Spotify Logout', 'width=700,height=500,top=40,left=40');
+    
+    setTimeout(() => {
+        spotifyLogoutWindow.close();
+        window.location.href = '/'; 
+    }, 1000);
+}
 
-    html2canvas(element).then(canvas => {
-        const link = document.createElement('a');
-        link.download = 'content.png';
-        link.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-        link.click();
+function downloadAsPNG() {
+   const element = document.getElementById('download');
+
+   html2canvas(element).then(canvas => {
+       const link = document.createElement('a');
+       link.download = 'content.png';
+       link.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+       link.click();
+   });
+}
+
+function downloadAsPNG() {
+    var container = document.getElementById('download');
+    html2canvas(container).then(function (canvas) {
+        var imageData = canvas.toDataURL('image/png');
+
+    
+        var downloadLink = document.createElement('a');
+        downloadLink.href = imageData;
+        downloadLink.download = 'downloaded-image.png';
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
     });
 }
+
